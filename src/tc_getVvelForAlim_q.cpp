5c21b5bb70a98360339cb05a3d6eefb0eeb60f7e
Christopher Wells <cwellsny@nycap.rr.com>
2018-12-01 15:38:37 -0500

    Implement test cases f-s

---
+++ src/tc_getVvelForAlim_q.cpp
@@ -0,0 +1,28 @@
+#include <cfloat>
+#include <cmath>
+#include <iostream>
+
+#include "component/Decider.h"
+
+int main() {
+    Decider decider;
+
+    Sense sense = Sense::UPWARD;
+    double altFt = 12345.56;
+    double vsepAtCpaFt = 4232.42;
+    double intrProjAltFt = 35009.27;
+    double rangeTauS = NAN;
+
+    try {
+        double actual = decider.getVvelForAlim(sense, altFt, vsepAtCpaFt, intrProjAltFt, rangeTauS);
+
+        std::cout << "Test failed" << "\n";
+        std::cout << "-----------" << "\n";
+        std::cout << "Expected: " << "FAIL" << "\n";
+        std::cout << "Actual:   " << actual << "\n";
+    } catch (...) {
+        std::cout << "Test passed" << "\n";
+    }
+
+    return 0;
+}

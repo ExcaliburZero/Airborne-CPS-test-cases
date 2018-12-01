2f3ee841feed86939964fbd80f3f135d84d11718
Christopher Wells <cwellsny@nycap.rr.com>
2018-12-01 15:58:04 -0500

    Implement test cases t-ae

---
+++ src/tc_getVvelForAlim_ac.cpp
@@ -0,0 +1,29 @@
+#include <cfloat>
+#include <cmath>
+#include <iostream>
+
+#include "component/Decider.h"
+
+int main() {
+    Decider decider;
+
+    Sense sense = Sense::UPWARD;
+    double altFt = 12345.56;
+    double vsepAtCpaFt = 4232.42;
+    double intrProjAltFt = std::nextafter(std::nextafter(70000.0, DBL_MAX), DBL_MAX);
+    double rangeTauS = 42222.22;
+
+    try {
+        double actual = decider.getVvelForAlim(sense, altFt, vsepAtCpaFt, intrProjAltFt, rangeTauS);
+
+        std::cout << "Test failed" << "\n";
+        std::cout << "-----------" << "\n";
+        std::cout << "Expected: " << "FAIL" << "\n";
+        std::cout << "Actual:   " << actual << "\n";
+    } catch (...) {
+        std::cout << "Test passed" << "\n";
+    }
+
+    return 0;
+}
+
---
+++ src/tc_getVvelForAlim_t.cpp
@@ -0,0 +1,28 @@
+#include <cfloat>
+#include <cmath>
+#include <iostream>
+
+#include "component/Decider.h"
+
+int main() {
+    Decider decider;
+
+    Sense sense = Sense::UPWARD;
+    double altFt = std::nextafter(70000.0, DBL_MAX);
+    double vsepAtCpaFt = 4232.42;
+    double intrProjAltFt = 35009.27;
+    double rangeTauS = 42222.22;
+
+    try {
+        double actual = decider.getVvelForAlim(sense, altFt, vsepAtCpaFt, intrProjAltFt, rangeTauS);
+
+        std::cout << "Test failed" << "\n";
+        std::cout << "-----------" << "\n";
+        std::cout << "Expected: " << "FAIL" << "\n";
+        std::cout << "Actual:   " << actual << "\n";
+    } catch (...) {
+        std::cout << "Test passed" << "\n";
+    }
+
+    return 0;
+}
